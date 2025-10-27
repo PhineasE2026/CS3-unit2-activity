50c25406fde3d1c895aa4641979fe2681eb00e35
finny <phineas.ehrlich2026@gbwl.org>
2025-10-27 16:35:37 +0000

    the most important activital change

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("hello world")
+import pandas as pd
+import numpy as np
 
+df = pd.read_csv('movies_metadata_v2.csv', encoding='iso-8859-1').dropna(axis=1, how='all')
+print(df.head())
+print(df.shape)
+print(df.info())
 
-if __name__ == "__main__":
-    main()
+# part B
+
+budget_df = df[df['budget'] > 1000000]
+print(budget_df.shape)
+
+budget_lookup = pd.Series()
